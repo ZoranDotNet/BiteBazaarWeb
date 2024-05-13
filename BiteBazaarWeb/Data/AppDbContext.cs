037ef5f19a4a68f0f06464cc41cf9122cecb7d6c
Zoran Matovic <matovic.zoran73@gmail.com>
2024-05-13 14:54:37 +0200

    Login and Register page updates

--- BiteBazaarWeb/Data/AppDbContext.cs
+++ BiteBazaarWeb/Data/AppDbContext.cs
@@ -14,5 +14,6 @@ namespace BiteBazaarWeb.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
