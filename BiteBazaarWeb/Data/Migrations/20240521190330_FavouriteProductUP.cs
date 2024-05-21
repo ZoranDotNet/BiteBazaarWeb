059860c19e4af7c728dca4c683cfd43761494cc0
roxzlir <emil.ch.nordin@gmail.com>
2024-05-21 22:06:06 +0200

    Zoran - Emil: Added favourite list

---
+++ BiteBazaarWeb/Data/Migrations/20240521190330_FavouriteProductUP.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BiteBazaarWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class FavouriteProductUP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavoriteProducts",
+                columns: table => new
+                {
+                    FavouriteProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FkProductId = table.Column<int>(type: "int", nullable: false),
+                    FkApplicationUserId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteProducts", x => x.FavouriteProductId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteProducts");
+        }
+    }
+}
