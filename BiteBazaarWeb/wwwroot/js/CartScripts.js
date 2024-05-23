81c9df9c0163a69cc9d162ed73b59e2b1a18f44c
Zoran Matovic <matovic.zoran73@gmail.com>
2024-05-23 14:29:21 +0200

    modal

---
+++ BiteBazaarWeb/wwwroot/js/CartScripts.js
@@ -0,0 +1,77 @@
+﻿$(document).ready(function () {
+    $(document).ready(function () {
+        $('#openModal').click(function () {
+            $.ajax({
+                url: '/Customer/Cart/Index',
+                type: 'GET',
+                success: function (data) {
+                    $('#cartDiv').html(data)
+                    $('#myModal').modal('show')
+                }
+            })
+        })
+    })
+})
+
+
+
+
+$(document).on('click', '#plusbutton', function () {
+    console.log('inne i function plus')
+    var cartId = $(this).data('id')
+    $.ajax({
+        url: '/Customer/Cart/Plus',
+        type: 'POST',
+        data: { id: cartId },
+        success: function (data) {
+            $('#cartDiv').html(data)
+
+        },
+        error: function (xhr, status, error) {
+            console.error('Error', error)
+        }
+    })
+})
+
+
+
+
+$(document).ready(function () {
+    $(document).on('click', '#minusbutton', function () {
+        console.log('inne i function minus')
+        var cartId = $(this).data('id')
+        $.ajax({
+            url: '/Customer/Cart/Minus',
+            type: 'POST',
+            data: { id: cartId },
+            success: function (data) {
+                $('#cartDiv').html(data)
+
+            },
+            error: function (xhr, status, error) {
+                console.error('Error', error)
+            }
+        })
+    })
+})
+
+
+
+$(document).ready(function () {
+    $(document).on('click', '#deletebutton', function () {
+        console.log('inne i function delete')
+        var cartId = $(this).data('id')
+        $.ajax({
+            url: '/Customer/Cart/Delete',
+            type: 'POST',
+            data: { id: cartId },
+            success: function (data) {
+                $('#cartDiv').html(data)
+
+            },
+            error: function (xhr, status, error) {
+                console.error('Error', error)
+            }
+        });
+    })
+})
